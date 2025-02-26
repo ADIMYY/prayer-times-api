f571017b51be9b886b20d4a6b88e4a72e4db7424
Abdelrahman Ali Elsayed <146658311+ADIMYY@users.noreply.github.com>
2025-02-26 19:38:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,18 +19,14 @@ app.get('/', (req, res) => {
         const longitude = parseFloat(process.env.LON);
 
         if (isNaN(latitude) || isNaN(longitude)) {
-            return res.status(400).json({ error: "إحداثيات غير صحيحة، تأكد من ملف .env" });
+            return res.status(400).json({ error: "Invalid latitude or longitude" });
         }
 
-        const coordinates = new Coordinates(latitude, longitude);
-
-        
+        const coordinates = new Coordinates(latitude, longitude);        
         const params = CalculationMethod.UmmAlQura();
 
-        
         const today = new Date();
         const prayerTimes = new PrayerTimes(coordinates, today, params);
-
         
         const formatTime = (time) => {
             return time ? time.toLocaleTimeString('en-GB', { timeZone: 'Asia/Riyadh', hour12: true }) : null;
